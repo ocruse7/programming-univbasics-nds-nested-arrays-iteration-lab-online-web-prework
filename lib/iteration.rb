fda6407f360037db22a307fea6f27e9868fc31cd
Owen Cruse <ocruse7@gmail.com>
2019-10-24 20:23:59 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -25,13 +25,13 @@ def find_greater_pair(src)
   larger_nums = []
   
   src.length.times do |index|
-    #if src[index][0] > src[index][1]
-      #larger_nums[index] = src[index][0]
-   # else if src[index][1] > src[index][0]
-    #  larger_nums[index] = src[index][1]
-    #else
-    #  larger_nums[index] = nil
-    #end
+    if src[index][0] > src[index][1]
+      larger_nums[index] = src[index][0]
+    else if src[index][1] > src[index][0]
+      larger_nums[index] = src[index][1]
+    else
+     larger_nums[index] = src[index][0]
+    end
   end
   
   larger_nums
